b5be154481ed26cd49458dedb7b1d398ee45ee22
F4briMD <f4brimd@hotmail.com>
2023-08-21 20:51:33 -0300

    init app

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,11 @@ function App() {
 
 
   return (
-    <main className='section'>
-      <Calc_iva/>
+    <main className='flex flex-col items-center justify-center h-screen max-w-6xl mx-auto text-4xl text-white items-cernter'>
+      <section>
+        <Calc_iva/>
+      </section>
+      
     </main>
   )
 }
--- src/components/Calc_iva.jsx
+++ src/components/Calc_iva.jsx
@@ -1,7 +1,7 @@
 import  { useState } from 'react';
 
 function Calc_iva() {
-  const [precio, setPrecio] = useState(0);
+  const [precio, setPrecio] = useState();
   const [tasaIVA, setTasaIVA] = useState(21); // Tasa de IVA en porcentaje
   const [iva, setIVA] = useState(0);
 
@@ -9,31 +9,42 @@ function Calc_iva() {
     const ivaCalculado = (precio * tasaIVA) / 100;
     setIVA(ivaCalculado);
   };
+  const calcularTotalConIVA = () => {
+    return precio + iva;
+  };
 
   return (
-    <div className='mx-auto '>
-      <h2 className='text-white'>Calculadora de IVA</h2>
-      <div>
+    <section className='max-w-4xl p-5 text-center'>
+      <h2 className='p-5'>Calculadora de IVA</h2>
+      <div className='grid grid-cols-2 gap-5 '>
+      <div className='flex flex-col items-center '>
         <label>Precio: </label>
         <input
           type="number"
           value={precio}
           onChange={(e) => setPrecio(parseFloat(e.target.value))}
+          className='mt-1 placeholderiva '
         />
       </div>
-      <div>
+      <div className='flex flex-col items-center'>
         <label>Tasa de IVA (%): </label>
         <input
           type="number"
           value={tasaIVA}
           onChange={(e) => setTasaIVA(parseFloat(e.target.value))}
+          className='mt-1 border-none placeholderiva focus:ring-cyan-400 focus:border-cyan-400'
         />
       </div>
-      <button onClick={calcularIVA}>Calcular IVA</button>
+      </div>
+      <button onClick={calcularIVA}
+      className='p-2 text-lg font-bold rounded-lg text-slate-600 bg-slate-200'>
+        Calcular IVA</button>
       <div>
         <h3>IVA Calculado: {iva.toFixed(2)}</h3>
+        <h2>Precio Total con IVA: {calcularTotalConIVA().toFixed(2)}</h2>
       </div>
-    </div>
+      
+    </section>
   );
 }
 
